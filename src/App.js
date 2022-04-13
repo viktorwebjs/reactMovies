2c0df6a62670b66d434e4bee5c97c63f4c767137
viktorwebjs <viktor.webjs@gmail.com>
2022-04-13 15:02:35 +0300

    +-

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <p>React </p>
-    </div>
-  );
+class App extends Component {
+  state = {
+    count: 0,
+  };
+
+  handleClickPlus = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  handleClickMinus = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div className="App" style={{ margin: '0 auto', width: '200px' }}>
+        <p>React </p>
+        <button onClick={this.handleClickPlus}>+</button>
+        <span style={{ margin: '0 0.75rem', display: 'inline-block' }}>
+          {this.state.count}
+        </span>
+        <button onClick={this.handleClickMinus}>- </button>
+      </div>
+    );
+  }
 }
 
 export default App;
